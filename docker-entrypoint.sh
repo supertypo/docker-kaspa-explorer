0cbe8294c8bd4529c6e39b5d81b3b8b615c521bc
supertypo <mthoring@protonmail.com>
2022-10-18 18:31:53 +0200

    Updated build.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ ! -f /tmp/.API_REPLACED ]; then
-  find build/ -type f -name '*.js' -exec sed -i -e "s|https://kaspa.herokuapp.com|${API_URL}|g" {} \;
-  find build/ -type f -name '*.js' -exec sed -i -e "s|wss://kaspa.herokuapp.com|${API_WSS_URL}|g" {} \;
+  find /app/ -name node_modules -prune -o -type f -exec sed -i "s|https://kaspa.herokuapp.com|${API_URI}|g" {} \;
+  find /app/ -name node_modules -prune -o -type f -exec sed -i "s|wss://kaspa.herokuapp.com|${API_WS_URI}|g" {} \;
   touch /tmp/.API_REPLACED
 fi
 exec /usr/bin/dumb-init -- "$@"
